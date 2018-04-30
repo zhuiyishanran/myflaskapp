db0c7f850e2341ac96e16b9d86e1b644756330dc
zhuiyishanran <awaitm@qq.com>
2018-04-30 20:03:34 +0800

    smtp

--- hello.py
+++ hello.py
@@ -20,7 +20,7 @@ app.config['SQLALCHEMY_DATABASE_URI']=\
     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['MAIL_SERVER'] = 'stmp.qq.com'
+app.config['MAIL_SERVER'] = 'smtp.qq.com'
 app.config['MAIL_PORT'] = 25
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
