d1bad3476b72f0671f7e33645e12a2ea9a6fb8e5
zhuiyishanran <awaitm@qq.com>
2018-05-04 22:03:56 +0800

    chapter 13

--- app/main/forms.py
+++ app/main/forms.py
@@ -51,3 +51,8 @@ class EditProfileAdminForm(FlaskForm):
         if field.data != self.user.username and \
                 User.query.filter_by(username=field.data).first():
             raise ValidationError('Username already in use.')
+
+
+class CommentForm(FlaskForm):
+    body = StringField('', validators=[Required()])
+    submit = SubmitField('Submit')
